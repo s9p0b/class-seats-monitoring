5a1c84df6f00bbc7ec02885019b2ff48bda3a604
s9p0b <asvatukhin@gmail.com>
2019-09-16 01:28:43 -0700

    Add continuous loop to check for avaliable seats in class

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
+import time
 
-response = requests.get('https://ubc-courses-api.herokuapp.com/2018W/CPSC/310/L1F')
-
-print(response.json())
+while True:
+    response = requests.get('https://ubc-courses-api.herokuapp.com/2018W/CPSC/310/L1F')
+    remainingSeats = response.json()['totalRemaining']
+    if (remainingSeats > 0):
+        print("Total seats currently remaining: " + str(remainingSeats))
+    time.sleep(10)
