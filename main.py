f6b331cfb570a07446b412b3e6b2be849f44100a
s9p0b <asvatukhin@gmail.com>
2019-09-16 01:29:19 -0700

    Add template to send email

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 import time
+import yagmail
+
+def sendEmail():
+    print("Sending an email...")
+    yag = yagmail.SMTP(FROM, 'pass')
+    yag.send(TO, SUBJECT, TEXT)
+    return
 
 while True:
     response = requests.get('https://ubc-courses-api.herokuapp.com/2018W/CPSC/310/L1F')
     remainingSeats = response.json()['totalRemaining']
     if (remainingSeats > 0):
         print("Total seats currently remaining: " + str(remainingSeats))
+        sendEmail()
     time.sleep(10)
