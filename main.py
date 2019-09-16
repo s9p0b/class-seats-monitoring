bbdc7030bb9cf3521631659685b610781687408d
s9p0b <asvatukhin@gmail.com>
2019-09-16 09:27:42 -0700

    Add smptlib email template

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import requests
 import time
-import yagmail
+import smtplib
 
 def sendEmail():
     print("Sending an email...")
-    yag = yagmail.SMTP(FROM, 'pass')
-    yag.send(TO, SUBJECT, TEXT)
-    return
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login("", "")
+    server.sendmail(
+            "",
+            "",
+            "Free seats avaliable for L1F")
+    server.quit()
+    print("Done")
 
 while True:
-    response = requests.get('https://ubc-courses-api.herokuapp.com/2018W/CPSC/310/L1F')
+    response = requests.get('https://ubc-courses-api.herokuapp.com/2019W/CPSC/310/L1F')
     remainingSeats = response.json()['totalRemaining']
-    if (remainingSeats > 0):
+    if (remainingSeats == 0):
         print("Total seats currently remaining: " + str(remainingSeats))
         sendEmail()
+        return
+    else:
+        print("The class is full :c")
     time.sleep(10)
