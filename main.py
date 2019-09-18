a989fe1ac50d1294ed508c0ab9e314d60f9e9b6b
s9p0b <asvatukhin@gmail.com>
2019-09-17 17:12:33 -0700

    Add message subject. Wrap code in main()

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ def sendEmail():
     print("Sending an email...")
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login("", "")
+    message = 'Subject: {}\n\n{}'.format("L1F: FREE SEAT", "Free seats avaliable for L1F")
     server.sendmail(
             "",
             "",
-            "Free seats avaliable for L1F")
+            message)
     server.quit()
     print("Done")
 
-while True:
-    response = requests.get('https://ubc-courses-api.herokuapp.com/2019W/CPSC/310/L1F')
-    remainingSeats = response.json()['totalRemaining']
-    if (remainingSeats == 0):
-        print("Total seats currently remaining: " + str(remainingSeats))
-        sendEmail()
-        return
-    else:
-        print("The class is full :c")
-    time.sleep(10)
+def main():
+    while True:
+        response = requests.get('https://ubc-courses-api.herokuapp.com/2019W/CPSC/310/L1F')
+        remainingSeats = response.json()['totalRemaining']
+        if (remainingSeats > 0):
+            print("Total seats currently remaining: " + str(remainingSeats))
+            sendEmail()
+            return
+        else:
+            print("The class is full :c")
+        time.sleep(60)
+
+main()
